691b3c345dc46639a90893f954f3802e24f9790b
matt chisholm <matt@theory.org>
2010-03-17 15:19:00 -0700

    updating URLs and email addresses to no longer include mosuki / devsuki domains, since those are going extinct soon.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(name='PottyMouth',
       py_modules=['PottyMouth'],
-      version='1.2.1',
+      version='1.2.2',
       data_files=[('share/doc/python-pottymouth', ['readme.html',
                                                    'LICENSE.txt',
                                                    'test.py'    ,
@@ -15,12 +15,12 @@ setup(name='PottyMouth',
                                                    'profile.py' ,]),],
       # metadata for fun
       author='Matt Chisholm',
-      author_email='matt@mosuki.com',
+      author_email='matt@theory.org',
       description="transform unstructured, untrusted text to safe, valid XHTML",
       license='BSD License',
       keywords='wiki',
-      url='http://devsuki.com/pottymouth',
-      download_url='http://devsuki.com/pottymouth/dist/',
+      url='http://glyphobet.net/pottymouth',
+      download_url='http://glyphobet.net/pottymouth/dist/',
       long_description="""PottyMouth transforms completely unstructured and untrusted text to valid, nice-looking, completely safe XHTML.
 
 PottyMouth is designed to handle input text from non-technical, potentially careless or malicious users. It produces HTML that is completely safe, programmatically and visually, to include on any web page. And you don't need to make your users read any instructions before they start typing. They don't even need to know that PottyMouth is being used.""",
