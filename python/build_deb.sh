f203416f657dabe14072246a4fe07555bd2961db
matt chisholm <matt@theory.org>
2011-11-20 17:17:34 +0100

    build_deb.sh now updates debian changelog; new command for how to upload to PyPi

--- python/build_deb.sh
+++ python/build_deb.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+set -e
 
+# update Debian changelog
+DEBEMAIL='matt@theory.org' dch -v `python -c 'import pottymouth; print pottymouth.__version__'`-0
+
+# remove old deb files
 rm ../python-pottymouth_* -f
+
+# build package
 debuild -uc -us
