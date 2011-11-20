e9f5c58ce8ee4b2d6af6a518d66a9b086f6ce299
matt chisholm <matt@theory.org>
2011-11-20 17:12:22 +0100

    shell script to build .zip file of documentation for uploading to PyPi

---
+++ python/pypi-docs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rm pypi-docs.zip
+cp ../readme.html index.html
+zip pypi-docs.zip index.html
+rm index.html
+echo "Now upload pypi-docs.zip to PyPi here: http://pypi.python.org/pypi?%3Aaction=pkg_edit&name=PottyMouth"
