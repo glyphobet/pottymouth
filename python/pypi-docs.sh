8eae65c0e3020eea2d94c57703d459c26645348c
matt chisholm <matt@theory.org>
2011-11-29 22:40:58 +0100

    better docs script

--- python/pypi-docs.sh
+++ python/pypi-docs.sh
@@ -4,3 +4,4 @@ cp ../readme.html index.html
 zip pypi-docs.zip index.html
 rm index.html
 echo "Now upload pypi-docs.zip to PyPi here: http://pypi.python.org/pypi?%3Aaction=pkg_edit&name=PottyMouth"
+echo "Remember to remove pypi-docs.zip when you're done."
