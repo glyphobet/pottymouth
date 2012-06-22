845a749e3def5b85af63453147fae412d11d70d8
Matt Chisholm <matt@theory.org>
2012-06-22 21:42:39 +0200

    javascript test generator

--- python/generate_javascript_tests.py
+++ python/generate_javascript_tests.py
@@ -1,17 +1,19 @@
+# Run with:
+# % python generate_javascript_tests.py > ../javascript/newtest.js && diffmerge ../javascript/test.js ../javascript/newtest.js
 import inspect
-
 from pottymouth import Node
-
 from test import basic_tests
 
 class JavaScriptTestGenerator(basic_tests):
     def _helper(self, source, expected):
         # import pdb; pdb.set_trace()
-        print u"""    equal(p.parse(%r).toString(),\n      %r,\n      '%s');""" % (
-            source,
-            str(Node('div', *expected)),
+        if isinstance(source, str):
+            source = source.decode('utf8')
+        print(u"""    equal(p.parse(%s).toString(),\n      %r,\n      '%s');""" % (
+            repr(source)[1:], # strip off the u prefix
+            str(Node('div', *expected)).decode('utf8').encode('ascii', errors='xmlcharrefreplace'),
             inspect.stack()[1][3]
-        )
+        ))
 
 if __name__ == '__main__':
     jstestgen = JavaScriptTestGenerator()
@@ -20,5 +22,4 @@ if __name__ == '__main__':
             try:
                 getattr(jstestgen, f)()
             except Exception as exc:
-                # raise
                 print "    // Skipped %s" % f
